cd9d4d2f9ecef8b33ee2f082c5357cf0b6059f00
paulords95 <paulords95@gmail.com>
2020-07-10 09:20:19 -0300

    CRUD tests

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,34 @@
 function MessageApp () {
     this.messages = []
+
+    //C
+    this.post = function (content) {
+        let item = {
+            id: this.messages.length,
+            content: content,
+            date: new Date()
+        }
+        this.messages.push(item)
+        return this.messages
+    }
+
+    //R
+    this.get = function (id) {
+        return this.messages[id]
+    }
+
+    //U
+    this.update = function (id, update) {
+        this.messages[id].content = update
+        return this.messages[id]
+    }
+
+    //D
+    this.delete = function (id) {
+        this.messages.splice(id, 1)
+        return this.messages
+    }
+
 }
 
 
